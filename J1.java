5d5d3af4b5ab254ccf7e41a3d07b2b43626db883
Emanthegiant <Emmanyoki@gmail.com>
2025-08-14 17:43:26 +0100

    Add files via upload

---
+++ J1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class j1
+
+{
+
+public static void main(String[] args) {
+Scanner in = new Scanner(System.in);
+int a;
+System.out.println("Enter number");
+a =in.nextInt();
+
+if(a>0)
+
+System.out.print("positive no");
+
+else
+
+System.out.print("negetive no");
+
+}
+
+}
