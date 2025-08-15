2c798802bf4ee185fa39a526eb8a38ee11ede92f
Emanthegiant <Emmanyoki@gmail.com>
2025-08-15 12:55:16 +0100

    Add files via upload

---
+++ j2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Main{
+    public static void main(Sring[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+        System.out.println("The string is"+str);
+        int num = scn.nextInt();
+        System.out.println("Number is"+num);
+        float fnum = scn.nextFloat();
+        Sysrem.out.println("The floating num"+fnum);
+    }
+}
---
+++ j3.java
@@ -0,0 +1,28 @@
+class J3 {
+
+public static void main(String[] args) {
+
+boolean sunny = false;
+
+boolean hot = true;
+
+if(sunny){
+
+if(hot){
+
+System.out.println("Use cap & half sleeves");
+
+}else{
+
+System.out.println("Use half sleeves");
+
+}
+
+}else{
+
+System.out.println("Take Rain-Coat for safety");
+
+}
+
+}
+}
